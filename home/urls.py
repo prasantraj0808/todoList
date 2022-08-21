30111978aee8f3889eb1d02d1b1f721ac78e8dbc
prasantraj0808 <prasantraj0808@gmail.com>
2022-08-22 03:24:40 +0530

    added models, registerd models, made migrations ,created superuser

--- home/urls.py
+++ home/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   
     path('',views.home, name='home'),
     path('tasks', views.tasks, name='tasks')
 
